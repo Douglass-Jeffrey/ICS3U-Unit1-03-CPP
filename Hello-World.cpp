a7661e0cfd66d9b9b3658ecbd6400453c78d90e0
Ubuntu <ubuntu@ip-172-31-19-198.ec2.internal>
2019-09-12 21:52:47 +0000

    ic

---
+++ Hello-World.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Douglass Jeffrey All rights reserved.
+//
+// Created by: Douglass Jeffrey
+// Created on: september 2019
+// This is a fancy version of the "Hello, World!" program.
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+}
